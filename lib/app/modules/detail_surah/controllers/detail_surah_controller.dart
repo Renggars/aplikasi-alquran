72213383fd515ab52681ceab35f65d613033e129
Rengga <renggarendy22@gmail.com>
2024-12-03 22:16:31 +0700

    Fix Play Audio

--- lib/app/modules/detail_surah/controllers/detail_surah_controller.dart
+++ lib/app/modules/detail_surah/controllers/detail_surah_controller.dart
@@ -9,6 +9,8 @@ import 'package:just_audio/just_audio.dart';
 class DetailSurahController extends GetxController {
   final player = AudioPlayer();
 
+  Verse? lastVerse;
+
   Future<DetailSurah> getDetailSurah(String id) async {
     Uri url = Uri.parse("https://api.quran.gading.dev/surah/$id");
     var res = await http.get(url);
@@ -23,6 +25,10 @@ class DetailSurahController extends GetxController {
     if (ayat?.audio?.primary != null) {
       // Catching errors at load time
       try {
+        lastVerse!.kondisiAudio = "stop";
+        lastVerse ??= ayat;
+        lastVerse!.kondisiAudio = "stop";
+        update();
         await player
             .stop(); //mencegah terjadi penumpukan audio yang sedang diputar
         await player.setUrl(ayat!.audio!.primary!);
